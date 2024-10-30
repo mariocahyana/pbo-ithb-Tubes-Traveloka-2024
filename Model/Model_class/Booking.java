23e9416ca0e5e4a9515f2a6edeec19ff445f91e7
mariocahyana <mariocahyana55@gmail.com>
2024-10-30 08:43:34 +0700

    flight and booking class

--- Model/Model_class/Booking.java
+++ Model/Model_class/Booking.java
@@ -1,5 +1,26 @@
 package Model_class;
 
 public class Booking {
+    private String bookingID;
+    private Customer customer;
+    private Flight flight;
     
+    public String getBookingID() {
+        return bookingID;
+    }
+    public void setBookingID(String bookingID) {
+        this.bookingID = bookingID;
+    }
+    public Customer getCustomer() {
+        return customer;
+    }
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+    public Flight getFlight() {
+        return flight;
+    }
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
 }
--- Model/Model_class/Flight.java
+++ Model/Model_class/Flight.java
@@ -1,5 +1,69 @@
 package Model_class;
 
+import Model_enum.*;
+
 public class Flight {
-    
+    private String flightNumber; 
+    private Airline airline; 
+    private int ticketCounter;
+    private Airport origin; 
+    private Airport destination; 
+    private String departureTime; 
+    private String arrivalTime; 
+    private FlightClass flightClass;
+    private double price;
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+    public Airline getAirline() {
+        return airline;
+    }
+    public void setAirline(Airline airline) {
+        this.airline = airline;
+    }
+    public int getTicketCounter() {
+        return ticketCounter;
+    }
+    public void setTicketCounter(int ticketCounter) {
+        this.ticketCounter = ticketCounter;
+    }
+    public Airport getOrigin() {
+        return origin;
+    }
+    public void setOrigin(Airport origin) {
+        this.origin = origin;
+    }
+    public Airport getDestination() {
+        return destination;
+    }
+    public void setDestination(Airport destination) {
+        this.destination = destination;
+    }
+    public String getDepartureTime() {
+        return departureTime;
+    }
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+    public FlightClass getFlightClass() {
+        return flightClass;
+    }
+    public void setFlightClass(FlightClass flightClass) {
+        this.flightClass = flightClass;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
